59898248fd6bf11b1c4c3a3e834a7a276bbe1515
Matt <matfsw@gmail.com>
2011-04-07 20:36:27 +0200

    Added support for groovy fixtures in New File dialog

--- unittest/org/concordion/ide/eclipse/wizards/NewSpecWizardTest.java
+++ unittest/org/concordion/ide/eclipse/wizards/NewSpecWizardTest.java
@@ -2,14 +2,16 @@ package org.concordion.ide.eclipse.wizards;
 
 import static org.junit.Assert.assertEquals;
 
+import org.concordion.ide.eclipse.template.FixtureTemplate.Language;
 import org.junit.Test;
 
 public class NewSpecWizardTest {
 
 	@Test
 	public void testTestCaseName() {
-		assertEquals("blaTest.java", NewSpecWizard.testCaseName("bla.html", "Test"));
-		assertEquals("blaTest.java", NewSpecWizard.testCaseName("bla", "Test"));
+		assertEquals("blaTest.java", NewSpecWizard.testCaseName("bla.html", "Test", Language.JAVA));
+		assertEquals("blaTest.java", NewSpecWizard.testCaseName("bla", "Test", Language.JAVA));
+		assertEquals("blaTest.groovy", NewSpecWizard.testCaseName("bla", "Test", Language.GROOVY));
 	}
 
 }
